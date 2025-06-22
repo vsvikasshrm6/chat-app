ef79e044521cd70ca1fa86bc8c203e099dbeb279
vsvikashrm6 <vsvikasshrm6@gmail.com>
2025-06-22 23:18:46 +0530

    firstIntegration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,8 @@ import HomePage from './Components/HomePage'
 import NavBar from './Components/NavBar'
 import { authStore } from './store/authStore';
 import {Loader} from "lucide-react"
+import { Toaster } from 'react-hot-toast'
+import LoginPage from './Components/login'
 
 function App() {
   const {authUser, checkAuth, isCheckingAuth} = authStore();
@@ -26,13 +28,12 @@ function App() {
       <NavBar/>
       <Routes>
         <Route path='/' element = {authUser? <HomePage/> : <Navigate to={"/login"}/>}></Route>
-        {/* <Route path='/' element = {<HomePage/>}></Route> */}
         <Route path='/settings' element ={<SettingsPage/>}></Route>
-        <Route path='/login' element = {<Login/>}></Route>
-        <Route path='/signup' element= {<SignUpPage/>}></Route>
+        <Route path='/login' element = {!authUser? <LoginPage/> : <Navigate to={"/"}/>}></Route>
+        <Route path='/signup' element= {!authUser? <SignUpPage/> : <Navigate to={"/"}/>}></Route>
         <Route path='/profile' element = {authUser ? <Profile/> : <Navigate to={"/login"}/>}></Route>
-        {/* <Route path='/profile' element = { <Profile/>}></Route> */}
       </Routes>
+      <Toaster></Toaster>
       
     </div>
   )
